921e065daa785950cb478b9817440255d61b5fff
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-11 14:24:12 -0700

    adding browser router and style imports

---
+++ movie-fan-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
+import './styles/style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App';
+
+
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
