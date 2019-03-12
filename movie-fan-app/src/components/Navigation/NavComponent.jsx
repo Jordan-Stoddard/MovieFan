c31d6ed6f32899fc269d16ba2d8af6fa845ef1ec
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-12 08:49:06 -0700

    added component to keep NavBar DRY

---
+++ movie-fan-app/src/components/Navigation/NavComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import {NavItem} from 'reactstrap'
+
+export default function NavComponent({navItem: {navTitle, path}}) {
+  return (
+    <div>
+       <NavItem>
+          <Link to={`/${path}`}><section>
+        <h3 className="varient-3">{navTitle}</h3>
+        <div>
+          <span className="hover hover-3">{navTitle}</span>
+        </div>
+      </section></Link>
+        </NavItem>
+    </div>
+  )
+}
