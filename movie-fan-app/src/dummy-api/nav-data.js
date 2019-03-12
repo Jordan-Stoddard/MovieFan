5bbe6434fd5cfafb1933a20dd0eb78a53c157318
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-12 08:49:28 -0700

    Added an array of objects to keep code DRY

---
+++ movie-fan-app/src/dummy-api/nav-data.js
@@ -0,0 +1,31 @@
+export const navDB = [{
+    navTitle: 'Discover',
+    path: '/discover',
+    id: Math.random()
+}, {
+    navTitle: 'Popular',
+    path: '/popular',
+    id: Math.random()
+}, {
+    navTitle: 'Top Rated',
+    path: '/top-rated',
+    id: Math.random()
+}, {
+    navTitle: 'input',
+    id: Math.random()
+}, {
+    navTitle: 'Upcoming',
+    path: '/upcoming',
+    id: Math.random()
+}, {
+    navTitle: 'Now Playing',
+    path: '/now-playing',
+    id: Math.random()
+}, {
+    navTitle: 'TV Shows',
+    path: '/tv-shows',
+    id: Math.random()
+},
+
+
+]
