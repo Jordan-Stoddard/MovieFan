1967d5e73f81b95e14942adbe4971c7defcf78e4
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-13 11:07:45 -0700

    added jumbotron

--- movie-fan-app/src/views/Home.jsx
+++ movie-fan-app/src/views/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
+import HomeCarousel from '../components/Carousels/HomeCarousel'
+import HomeJumboTron from '../components/HomeComponents/HomeJumbotron'
+import axios from 'axios'
+
 
 export default function Home() {
+  const [trending, setTrending] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.themoviedb.org/3/trending/movie/week?api_key=ce7c59deb6a7bf9b40fc57ad9e1ff469&sort_by=popularity.desc')
+    .then(res => setTrending(res.data.results))
+  }, [])
+
+
   return (
     <div>
-      Home
+    {trending.length !== 0 ? <HomeCarousel trending={trending}/> : <div>loading...</div>}
+    <HomeJumboTron fluid/>
+    
     </div>
-  )
+  );
 }
