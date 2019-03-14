3c2dd812050910d7295adff74afbea2426a76452
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-14 11:05:18 -0700

    changed styles

--- movie-fan-app/src/components/HomeComponents/HomeJumbotron.js
+++ movie-fan-app/src/components/HomeComponents/HomeJumbotron.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Jumbotron, Container } from "reactstrap";
+import { Jumbotron } from "reactstrap";
+
+// Styles found in _homePageStyles
 
 const HomeJumbotron = props => {
   return (
