aac5721b116e85b82d3ae34f21bc935d98efc4c2
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-16 15:12:43 -0700

    added dependencies

--- movie-fan-app/src/App.js
+++ movie-fan-app/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import NavBar from './components/Navigation/NavBar'
-import Discover from './views/Discover'
 import NowPlaying from './views/NowPlaying'
 import Popular from './views/Popular'
 import TopRated from './views/TopRated'
 import TVShows from './views/TVShows'
 import Upcoming from './views/Upcoming'
 import Home from './views/Home'
+import Footer from './components/Footer/Footer'
 
 
 export default function App() {
@@ -16,7 +16,6 @@ export default function App() {
       <NavBar/>
     <Switch>
     <Route path="/" exact component={Home}/>
-    <Route path="/discover" component={Discover}/>
     <Route path="/popular" component={Popular}/>
     <Route path="/top-rated" component={TopRated}/>
     <Route path="/upcoming" component={Upcoming}/>
@@ -25,6 +24,7 @@ export default function App() {
     <Route path="/details/movie/:id" component={Home}/>
     <Route path="/details/tv/:id" component={Home}/>
     </Switch>
+    <Footer />
     </div>
   )
 }
