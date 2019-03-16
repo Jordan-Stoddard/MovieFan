3c2f25fc9f36475446f74510d06bd06c359e3d4f
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-16 15:14:04 -0700

    added banner

--- movie-fan-app/src/views/NowPlaying.jsx
+++ movie-fan-app/src/views/NowPlaying.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import TopBanner from '../components/Banners/TopBanner'
+import image from '../assets/beach-dawn-dusk-1144176.jpg'
 
 export default function NowPlaying() {
   return (
     <div>
-      NowPlaying
+      <TopBanner title="NOW PLAYING" image={image}/>
     </div>
   )
 }
