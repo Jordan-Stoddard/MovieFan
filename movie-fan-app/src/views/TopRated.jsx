358e1e2c37886b7cd9760aa5d97121646a7cda7e
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-18 09:49:14 -0700

    testing DRY code

--- movie-fan-app/src/views/TopRated.jsx
+++ movie-fan-app/src/views/TopRated.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
+import TopBanner from '../components/Banners/TopBanner'
+import image from '../assets/beach-idyllic-island-690303.jpg'
+import {useGetTopic} from '../dummy-api/fetchingHooks'
+import ButtonContainer from '../components/Buttons/ButtonContainer'
+import TopicDisplay from '../components/TopicDisplay/TopicDisplay'
 
-export default function TopRated() {
+export default function TopRated({topic}) {
+  const [page, setPage] = useState(1)
+    const getMovie = useGetTopic(topic, page)
   return (
     <div>
-      TopRated
+      <TopBanner title='TOP RATED' image={image}/>
+      <div className="separator"></div>
+      <ButtonContainer />
+      <TopicDisplay getMovie={getMovie}/>
     </div>
   )
 }
