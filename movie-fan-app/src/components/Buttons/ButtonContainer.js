7cb7af38b00bf3ffa1963dd7807286e378bbbc49
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-18 09:49:32 -0700

    initialized component

---
+++ movie-fan-app/src/components/Buttons/ButtonContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ButtonContainer() {
+  return (
+    <div className="button-container">
+      <button className="page-button-1"><p>PREV</p></button>
+      <button className="page-button-2"><p>NEXT</p></button>
+    </div>
+  )
+}
