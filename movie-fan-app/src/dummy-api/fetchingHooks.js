40a51035d782750c21fc1f1bf0a705996fffbbed
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-18 09:46:49 -0700

    testing useGetGenres

--- movie-fan-app/src/dummy-api/fetchingHooks.js
+++ movie-fan-app/src/dummy-api/fetchingHooks.js
@@ -1,59 +1,35 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import { useState, useEffect } from 'react'
+import axios from "axios";
 
-export const usePopular = () => {
-    const [popularMovies, setPopularMovies] = useState([])
 
-    useEffect(() => {
+export const useGetTopic = (topic, page) => {
+  const [getMovies, setGetMovies] = useState([]);
 
-        (() => {
-           axios.get('https://api.themoviedb.org/3/movie/popular?api_key=ce7c59deb6a7bf9b40fc57ad9e1ff469&language=en-US&page=1')
-           .then(res => setPopularMovies(res.data.results))
-           .catch(err => console.log(err))
-        })()
-    }, [])
-    return popularMovies
+  useEffect(() => {
+    (() => {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${topic}?api_key=ce7c59deb6a7bf9b40fc57ad9e1ff469&language=en-US&page=${page}`
+        )
+        .then(res => setGetMovies(res.data.results))
+        .catch(err => console.log(err));
+    })();
+  }, []);
+  return getMovies;
+};
+
+export const useGetGenres = () => {
+  const [genre, setGenre] = useState([])
+
+  useEffect(() => {
+    (() => {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/genre/movie/list?api_key=ce7c59deb6a7bf9b40fc57ad9e1ff469&language=en-US`
+        )
+        .then(res => setGenre(res.data))
+        .catch(err => console.log(err));
+    })();
+  }, []);
+  return genre;
 }
-
-export const useTopRated = () => {
-    const [topRated, setTopRated] = useState([])
-
-    useEffect(() => {
-
-        (() => {
-           axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=ce7c59deb6a7bf9b40fc57ad9e1ff469&language=en-US&page=1')
-           .then(res => setTopRated(res.data.results))
-           .catch(err => console.log(err))
-        })()
-    }, [])
-    return topRated
-}
-
-export const useUpcoming = () => {
-    const [upComing, setUpComing] = useState([])
-
-    useEffect(() => {
-
-        (() => {
-           axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=ce7c59deb6a7bf9b40fc57ad9e1ff469&language=en-US&page=1')
-           .then(res => setUpComing(res.data.results))
-           .catch(err => console.log(err))
-        })()
-    }, [])
-    return upComing
-}
-
-export const useNowPlaying = () => {
-    const [nowPlaying, setNowPlaying] = useState([])
-
-    useEffect(() => {
-
-        (() => {
-           axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=ce7c59deb6a7bf9b40fc57ad9e1ff469&language=en-US&page=1')
-           .then(res => setNowPlaying(res.data.results))
-           .catch(err => console.log(err))
-        })()
-    }, [])
-    return nowPlaying
-}
-
