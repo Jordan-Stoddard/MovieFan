0935bc591e9f1b91cf55157aba8551995b2389a1
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-19 14:26:11 -0700

    added MovieDetailJumboTron component

---
+++ movie-fan-app/src/components/MovieDetailComponents/MovieDetailJumboTron.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { style, homepage } from "../../helper-functions/displayFunctions";
+
+export default function MovieDetailJumboTron({ movie, history }) {
+  return (
+    <div>
+      <div className="movie-detail-jumbo-img" style={style(movie)}>
+
+        <div className="movie-detail-jumbo-info-container">
+          <i
+            onClick={history.goBack}
+            className="fas fa-angle-left fa-5x back-arrow"
+          />
+
+          <div className="movie-detail-jumbo-content">
+            <img
+              src={`https://image.tmdb.org/t/p/w92${movie.poster_path}`}
+              alt={movie.title}
+              className="movie-detail-jumbo-poster"
+            />
+
+            <div className="movie-detail-jumbo-text">
+              <h1 className="movie-detail-jumbo-title">{movie.title}</h1>
+              <p className="movie-detail-jumbo-rating">{`Rating | ${movie.vote_average} out of 10`}</p>
+              <p className="movie-detail-jumbo-link">{homepage(movie)}</p>
+              <p className="movie-detail-jumbo-tagline">Tagline: {movie.tagline}</p>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
