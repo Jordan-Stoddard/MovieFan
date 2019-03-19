c56b56cca1a472cadb0c9a36efd5e88509a61057
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-19 14:28:25 -0700

    added functionality to button container

--- movie-fan-app/src/components/Buttons/ButtonContainer.js
+++ movie-fan-app/src/components/Buttons/ButtonContainer.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
+import {nextPage, prevPage} from '../../helper-functions/onClick'
 
-export default function ButtonContainer() {
+
+export default function ButtonContainer({page, setPage}) {
   return (
     <div className="button-container">
-      <button className="page-button-1"><p>PREV</p></button>
-      <button className="page-button-2"><p>NEXT</p></button>
+      <h1>{page}</h1>
+      <button onClick={(e) => prevPage(e, page, setPage)} className="page-button-1">
+        <p>PREV</p>
+      </button>
+      <button onClick={e => nextPage(e, page, setPage)} className="page-button-2">
+        <p>NEXT</p>
+      </button>
     </div>
-  )
+  );
 }
