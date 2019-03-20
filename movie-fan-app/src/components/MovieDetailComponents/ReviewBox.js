703e2ab366ac54ac1991a930b3585e238d2b540d
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-20 13:01:12 -0700

    initialized component

---
+++ movie-fan-app/src/components/MovieDetailComponents/ReviewBox.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { reviewContent } from '../../helper-functions/displayFunctions'
+
+
+export default function ReviewBox({review, index}) {
+  return (
+    <div className="review-box-container">
+      <div className="review-box" style={index % 2 ? styleEven : styleOdd}>
+      <h1 className="review-box-title">{review.author}</h1>
+      <p className="review-box-text">{reviewContent(review.content)}</p>
+      <a target="_blank" rel="noreferrer noopener" href={review.url}  className="review-box-link">See full review <i className="fas fa-external-link-alt"></i></a>
+      </div>
+    </div>
+  )
+}
+
+const styleEven = {
+    background: `linear-gradient(30deg, rgba(255, 255, 255, 0.003) 2%, #202020 88%)`
+}
+
+const styleOdd = {
+    background: `linear-gradient(190deg, rgba(255, 255, 255, 0.003) 2%, #202020 88%)`
+}
