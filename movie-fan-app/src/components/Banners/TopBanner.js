2e19b848603239eae9620d3b0df81f70c4ada5d3
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-20 13:00:09 -0700

    refactored to make code DRY

---
+++ movie-fan-app/src/components/Banners/TopBanner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TopBanner({title, image}) {
+  return (
+    <div>
+        <div className="title-banner" style={{backgroundImage: `url(${image})`}}>
+            <h1 className="banner-text">{title}</h1>
+        </div>
+    </div>
+  )
+}
