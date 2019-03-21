1f0502246b7dd82223f74fa22d8a938fa3bb3aa8
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-21 11:05:30 -0700

    changed styles

--- movie-fan-app/src/components/MovieDetailComponents/MovieDetailReviews.js
+++ movie-fan-app/src/components/MovieDetailComponents/MovieDetailReviews.js
@@ -5,6 +5,7 @@ export default function MovieDetailReviews({reviews}) {
   return (
     <div className="trailer-container">
       <h1 className="trailer-title">POPULAR REVIEWS</h1>
+      {reviews.length === 0 && <ReviewBox text="No reviews found."/>}
       {reviews.map((review, index) => 
         <ReviewBox review={review} key={review.id} index={index}/>
         )}
