94a15420b8072f085605af883429fd576dd71e1c
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-21 11:06:59 -0700

    initialized component

---
+++ movie-fan-app/src/views/PeopleDetailPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {useGetPerson, useGetPersonRoles} from '../dummy-api/fetchingHooks'
+import PersonDetailJumboTron from '../components/PersonDetailComponents/PersonDetailJumboTron'
+import PersonDetailSummary from '../components/PersonDetailComponents/PersonDetailSummary'
+import PersonDetailPopularRoles from '../components/PersonDetailComponents/PersonDetailPopularRoles'
+
+export default function PeopleDetailPage(props) {
+  const id = props.match.params.id
+  const person = useGetPerson(id)
+  const personRoles = useGetPersonRoles(id)
+
+  return (
+    <div>
+      <PersonDetailJumboTron person={person} personRoles={personRoles} {...props}/>
+      <div className="movie-detail-content-container">
+      <PersonDetailSummary person={person}/>
+      <PersonDetailPopularRoles personRoles={personRoles}/>
+      </div>
+    </div>
+  )
+}
