20e51683ca1bd23b89dd90e51d49fff89f5c4d49
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-21 11:06:09 -0700

    added comments for clarity

--- movie-fan-app/src/dummy-api/fetchingHooks.js
+++ movie-fan-app/src/dummy-api/fetchingHooks.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from "axios";
 
-
+//////// MOVIE END POINTS /////////
 export const useGetTopic = (topic, page) => {
   const [getMovies, setGetMovies] = useState([]);
 
@@ -84,4 +84,38 @@ export const useGetReviews = (id) => {
     })()
   }, [])
   return getReviews
+}
+
+export const useGetPerson = (id) => {
+  const [getPerson, setGetPerson] = useState([])
+
+  useEffect(() => {
+    (() => {
+      axios.get(
+        `https://api.themoviedb.org/3/person/${id}?api_key=ce7c59deb6a7bf9b40fc57ad9e1ff469&language=en-US`
+      )
+      .then(res => {
+        setGetPerson(res.data)
+      })
+      .catch(err => console.log(err))
+    })()
+  }, [])
+  return getPerson
+}
+
+export const useGetPersonRoles = (id) => {
+  const [getPersonRoles, setGetPersonRoles] = useState([])
+
+  useEffect(() => {
+    (() => {
+      axios.get(
+        `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=ce7c59deb6a7bf9b40fc57ad9e1ff469&language=en-US`
+      )
+      .then(res => {
+        setGetPersonRoles(res.data.cast)
+      })
+      .catch(err => console.log(err))
+    })()
+  }, [])
+  return getPersonRoles
 }
