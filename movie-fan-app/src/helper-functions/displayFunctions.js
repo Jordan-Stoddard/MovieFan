7e0d92adce688c87f7a9d9496777b657fcf935cd
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-21 11:06:38 -0700

    added display function

--- movie-fan-app/src/helper-functions/displayFunctions.js
+++ movie-fan-app/src/helper-functions/displayFunctions.js
@@ -1,16 +1,19 @@
 import React from 'react'
 
 export const style = (movie) => {
+  if (!movie) {
+    return {background: 'transparent'}
+  }
     return {background: `linear-gradient(0deg, rgb(0, 0, 0) 5%, rgba(0, 0, 0, 0.45) 92%) center center no-repeat, url(https://image.tmdb.org/t/p/original${
       movie.backdrop_path
     }) no-repeat center center`}
   }
 
-  export const homepage = (movie) => {
-    if (!movie.homepage) {
+  export const homepage = (object) => {
+    if (!object.homepage) {
       return  "No homepage found"
     } else {
-        return <a href={movie.homepage} target="_blank" rel="noreferrer noopener">{movie.homepage}</a>
+        return <a href={object.homepage} target="_blank" rel="noreferrer noopener">{object.homepage}</a>
     }
 }
 
@@ -28,4 +31,12 @@ export const reviewContent = (str) => {
     }
   }
   return `${newStr}...`
+}
+
+export const summaryView = (str) => {
+  if (!str) {
+    return <p className="summary-text-none">There is no biography for this person in our database.</p>
+  } else {
+    return <p className="summary-text">{str}</p>
+  }
 }
