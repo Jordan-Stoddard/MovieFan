c0b5331d5c2e2523c962183bccaf0b4e1ab965d2
Jordan Stoddard <jjsincorporated@gmail.com>
2019-03-22 12:56:47 -0700

    initialized component

--- movie-fan-app/src/views/TVShows.jsx
+++ movie-fan-app/src/views/TVShows.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import HomeCarousel from '../components/Carousels/HomeCarousel'
+import SmallTVCarousel from '../components/Carousels/SmallTVCarousel'
+import {useGetTVTrending} from '../dummy-api/fetchingTVHooks'
+
 
 export default function TVShows() {
+  const trending = useGetTVTrending()
   return (
     <div>
-      TVShows
+      <HomeCarousel trending={trending} topic="tv"/>
+      <div className="home-carousel-container">
+        <SmallTVCarousel topic="airing_today" page="1" genre="AIRING TODAY" media="tv"/>
+        <SmallTVCarousel topic="on_the_air" page="1" genre="ON THE AIR" media="tv"/>
+        <SmallTVCarousel topic="popular" page="1" genre="POPULAR" media="tv"/>
+        <SmallTVCarousel topic="top_rated" page="1" genre="TOP RATED" media="tv"/>
+      </div>
     </div>
   )
 }
