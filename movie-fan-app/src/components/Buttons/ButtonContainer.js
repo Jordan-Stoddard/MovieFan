fb0ded22fcdfb06cff9be73e8918cc3156d1ddc5
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-05 16:29:58 -0700

    fixed upcoming button page logic

--- movie-fan-app/src/components/Buttons/ButtonContainer.js
+++ movie-fan-app/src/components/Buttons/ButtonContainer.js
@@ -2,14 +2,13 @@ import React from "react";
 import {nextPage, prevPage} from '../../helper-functions/onClick'
 
 
-export default function ButtonContainer({page, setPage}) {
+export default function ButtonContainer({page, setPage, topic}) {
   return (
     <div className="button-container">
-      <h1>{page}</h1>
-      <button onClick={(e) => prevPage(e, page, setPage)} className="page-button-1">
+      <button onClick={(e) => prevPage(e, page, setPage, topic)} className="page-button-1">
         <p>PREV</p>
       </button>
-      <button onClick={e => nextPage(e, page, setPage)} className="page-button-2">
+      <button onClick={(e) => nextPage(e, page, setPage, topic)} className="page-button-2">
         <p>NEXT</p>
       </button>
     </div>
