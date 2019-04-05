c35c99d1543c88bf6a3802f8e01d10ec5a7ebab6
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-05 16:30:28 -0700

    added case for upcoming topic

--- movie-fan-app/src/helper-functions/onClick.js
+++ movie-fan-app/src/helper-functions/onClick.js
@@ -1,9 +1,17 @@
-export const nextPage = (e, page, setPage) => {
+export const nextPage = (e, page, setPage, topic) => {
   e.preventDefault();
-  page === 20 ? setPage(1) : setPage(page + 1);
+  if (topic === "upcoming") {
+    page === 10 ? setPage(1) : setPage(page + 1);
+  } else {
+    page === 20 ? setPage(1) : setPage(page + 1);
+  }
 };
 
-export const prevPage = (e, page, setPage) => {
+export const prevPage = (e, page, setPage, topic) => {
   e.preventDefault();
-  page === 1 ? setPage(20) : setPage(page - 1);
+  if (topic === "upcoming") {
+    page === 1 ? setPage(10) : setPage(page - 1);
+  } else {
+    page === 1 ? setPage(20) : setPage(page - 1);
+  }
 };
