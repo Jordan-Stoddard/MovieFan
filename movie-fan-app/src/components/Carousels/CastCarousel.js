0e3f46dfb95bb186385aae334e75f364b28b2154
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-06 10:26:45 -0700

    added cond render based on size of array from api

--- movie-fan-app/src/components/Carousels/CastCarousel.js
+++ movie-fan-app/src/components/Carousels/CastCarousel.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Slider from 'react-slick'
 import CastSlide from './CastSlide'
+import StaticCast from './StaticCast'
+
 
 export default function CastCarousel({cast}) {
-  
+  if (cast.length < 6) {
+    return (
+      <StaticCast cast={cast}/>
+    )
+  }
   return (
     <div className="small-carousel">
       <h2 className="small-carousel-genre">CAST</h2>
-      <Slider slidesToShow={5} infinite className="center" swipeToSlide>
+      <Slider slidesToShow={6} infinite className="center" swipeToSlide>
       {cast.map(cast => 
         <CastSlide cast={cast} key={cast.id}/>)}
       </Slider>
