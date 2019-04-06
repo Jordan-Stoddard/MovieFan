caac783e016c2d78638e2ff7769e5a619109ce53
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-06 10:27:45 -0700

    changed styles

--- movie-fan-app/src/components/Navigation/SearchInput.js
+++ movie-fan-app/src/components/Navigation/SearchInput.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
+import {withRouter} from 'react-router-dom'
 
-export default function SearchInput() {
+function SearchInput({setInputStateFunc, history, location}) {
+  const [searchInput, setSearchInput] = useState("");
+
+  const changeHandler = (e) => {
+    setSearchInput(e.target.value)
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    setInputStateFunc(searchInput)
+    pathName === '/tv-shows' || pathName === '/search-tv' || pathName.includes('/details/tv') ? history.push('/search-tv') : history.push('/search')
+    setSearchInput("")
+  }
+
+  const pathName = location.pathname
+
   return (
-    <div>
-       <input className="nav-bar-search" placeholder="Search..." />
+    <div className="nav-bar-search-container">
+      <form className="nav-bar-form" onSubmit={submitHandler}>
+      <input
+        className="nav-bar-search"
+        placeholder={pathName === '/tv-shows' || pathName === '/search-tv' || pathName.includes('/details/tv') ? `Search TV shows...` : `Search movies...`}
+        value={searchInput}
+        onChange={changeHandler}
+      />
+     <button className="search-svg"><i className="fas fa-search "/></button>
+      </form>
     </div>
-  )
+  
+  );
 }
+
+export default withRouter(SearchInput)
