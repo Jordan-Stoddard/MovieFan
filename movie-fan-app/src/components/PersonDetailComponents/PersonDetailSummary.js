ce99f1d511b0ac96d5ebe2de19c31ec19d1a7a4c
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-06 10:28:18 -0700

    added biographyview display function

--- movie-fan-app/src/components/PersonDetailComponents/PersonDetailSummary.js
+++ movie-fan-app/src/components/PersonDetailComponents/PersonDetailSummary.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {summaryView} from '../../helper-functions/displayFunctions'
+import {biographyView} from '../../helper-functions/displayFunctions'
 
 export default function PersonDetailSummary({person: {biography}}) {
     return (
       <div className="summary-container">
         <h1 className="summary-title">Biography</h1>
-        {summaryView(biography)}
+        {biographyView(biography)}
       </div>
     )
   }
