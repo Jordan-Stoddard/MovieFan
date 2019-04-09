2700bbd85457fb5a51e6dddee6561eb141f13bbd
Jordan Stoddard <jjsincorporated@gmail.com>
2019-04-09 12:39:41 -0700

    adds comments for clarity

--- movie-fan-app/src/views/Popular.jsx
+++ movie-fan-app/src/views/Popular.jsx
@@ -5,6 +5,8 @@ import { useGetTopic } from "../dummy-api/fetchingHooks";
 import ButtonContainer from "../components/Buttons/ButtonContainer";
 import TopicDisplay from "../components/TopicDisplay/TopicDisplay";
 
+// displays popular page
+
 export default function Popular({ topic }) {
   const [page, setPage] = useState(1);
   const getMovie = useGetTopic(topic, page);
